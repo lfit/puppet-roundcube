85225cbd2c5a7e3e4a073cda6ddf192faa9b6e17
Ryan F Day <rday@linuxfoundation.org>
2015-11-09 14:22:28 -0800

    forgot to put anything in params.pp.  fixed.
    Signed-off-by: Ryan F Day <rday@linuxfoundation.org>

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,4 +1,41 @@
+# == Class: roundcube::params
+#
+# === Parameters
+#
+# === Variables
+#
+# === Examples
+#
+# === Authors
+#
+# Ryan Finnin Day <rday@linuxfoundation.org>
+#
+# === Copyright
+#
+# Copyright 2015 Ryan Finnin Day
+
 class roundcube::params {
+
+  $db_provider      = 'mysql'
+  $db_user          = 'lfroundcube'
+  $db_pass          = 'password'
+  $db_host          = 'db.finninday.net'
+  $db_dbname        = 'lfmail'
+  $db_prefix        = 'rc_'
+  $debug_level      = '5'
+  $imap_url         = 'ssl://mail.finninday.net'
+  $imap_port        = '993'
+  $smtp_host        = 'mail.finninday.net'
+  $smtp_port        = '587'
+  $smtp_user        = ''
+  $smtp_pass        = ''
+  $support_url      = 'http://finninday.net/wiki/index.php/Roundcube'
+  $auto_create_user = true
+  $server_cert      = 'newcert.pem'
+  $timezone         = 'America/Los_Angeles'
+  $des_key          = 'OabdapIcujHasHadfankEsof'
+  $verify_peer      = false
+  $verify_peer_name = false
 
   case $::osfamily {
     'RedHat': {
