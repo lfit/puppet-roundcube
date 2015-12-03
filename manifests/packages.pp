bf452867eaf3146e1ddbc38b616e54df38c0d73f
Ryan F Day <rday@linuxfoundation.org>
2015-12-03 15:40:29 -0800

    Make dependency on apache, epel explicit. Clean up package installs using stdlib idiom.
    Signed-off-by: Ryan F Day <rday@linuxfoundation.org>

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -1,19 +1,16 @@
 class roundcube::packages {
-  package { 'epel-release':
-    ensure => 'installed',
-  }
-  package { 'roundcubemail':
-    name   => "${::roundcube::params::package_name}",
-    ensure => 'installed',
-    require => Package['epel-release'],
-  }
-  package { 'httpd':
-    ensure => 'installed',
-  }
-  package { 'php-mysql':
-    ensure => 'installed',
-  }
-  package { 'php-mcrypt':
-    ensure => 'installed',
-  }
+
+  include ::epel
+
+  ensure_resource('package', 
+    [
+      'httpd',
+      'php-mysql',
+      'php-mcrypt',
+      'roundcube'
+    ],
+    {
+      'ensure' => 'installed'
+    }
+  )
 }
